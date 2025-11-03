1987937ae0a984650653b70252b2b303c72baf0e
James McGhee <31243736+JamesMcGhee@users.noreply.github.com>
2025-11-03 15:25:12 +0000

    Project Creation
    - Created "empty" project
    - Added DataLens Blueprint Library plugin

---
+++ UE/DataLensPlugin/Plugins/DataLens/Source/DataLens/Private/DataLensBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DataLensBPLibrary.h"
+#include "DataLens.h"
+
+UDataLensBPLibrary::UDataLensBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UDataLensBPLibrary::DataLensSampleFunction(float Param)
+{
+	return -1;
+}
+
